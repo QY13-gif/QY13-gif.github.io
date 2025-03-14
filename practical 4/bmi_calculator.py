d43b57ddf2dc9e8d1b984cc6c03121f6cd868e37
QY13-gif <qiyang.24@intl.jzu.edu.cn>
2025-03-14 11:17:39 +0800

    update 'c:\Users\cqy111\Desktop\IBI1\QY13-gif.github.io\practical 4'

--- practical 4/bmi_calculator.py
+++ practical 4/bmi_calculator.py
@@ -1,6 +1,10 @@
+#Define weight as the number the user inputs, with the unit as kg, can be decimal friction
 weight=float(input("your weight(kg):"))
+#Define height as the number the user inputs, with the unit as m, can be decimal friction
 height=float(input("your height(m):"))
+#Define BMI 
 BMI=weight/(height**2)
+#Calculate BMI of the user, show the situation of the user's weight according to the BMI
 if BMI<18.5:
     category="under weight"
 elif 18.5<=BMI<30:
